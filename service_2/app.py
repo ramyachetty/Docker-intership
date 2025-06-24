1695b0b2d6ec6ad4dd0f047dc240a61c50456df2
root <root@ip-172-31-0-166.ap-southeast-2.compute.internal>
2025-06-24 07:31:54 +0000

    updated

--- service_2/app.py
+++ service_2/app.py
@@ -12,6 +12,10 @@ def ping():
 def hello():
     return jsonify(message="Hello from Service 2")
 
+@app.route("/")
+def index():
+    return jsonify(message="Welcome to Service 2 root!")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8002)
